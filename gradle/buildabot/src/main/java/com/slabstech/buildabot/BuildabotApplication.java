92fa163af9be668b18542eade5bc59f4cd4f0901
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2022-12-17 12:10:12 +0100

    Add gradle build

---
+++ gradle/buildabot/src/main/java/com/slabstech/buildabot/BuildabotApplication.java
@@ -0,0 +1,13 @@
+package com.slabstech.buildabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildabotApplication.class, args);
+	}
+
+}
---
+++ gradle/buildabot/src/test/java/com/slabstech/buildabot/BuildabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slabstech.buildabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
